808553080dd8d9797c3b9180bb90e15cbfb442ca
rambo <91745597+pranavxdev@users.noreply.github.com>
2022-08-07 19:20:07 +0400

    updated

--- main.js
+++ main.js
@@ -5,33 +5,46 @@ addNote.addEventListener("click", function () {
   const modal = document.getElementById("modal");
 
   //create note element
-  const note = document.createElement("div");
-  note.setAttribute("id", "note");
+  const tile = document.createElement("div");
+  tile.setAttribute("id", "note");
 
   // create note title
-  const title = document.createElement("h3");
+  const title = document.createElement("div");
   title.setAttribute("id", "title");
+  title.setAttribute("contentEditable", "false");
   title.innerHTML = "title";
 
+  // create text title
   const text = document.createElement("div");
   text.setAttribute("id", "text");
   text.innerHTML = "no additional text";
 
-  note.appendChild(title);
-  note.appendChild(text);
-  noteField.appendChild(note);
+  //append title and text into parent NOTE and append NOTE into NoteField
+  tile.appendChild(title);
+  tile.appendChild(text);
+  noteField.appendChild(tile);
 
-  note.onclick = function () {
-    note.classList.add("viewer");
-    note.style.width = "800px";
-    note.style.height = "500px";
+  // event listener when tile is clicked
+
+  tile.addEventListener("click", function () {
     modal.style.display = "initial";
-  };
+    tile.classList.add("viewer");
+    tile.style.width = "800px";
+    tile.style.height = "500px";
+    text.classList.add("text-viewer");
+    title.setAttribute("contentEditable", "true");
+    text.setAttribute("contentEditable", "true");
+  });
 
-  modal.onclick = function () {
-    note.classList.remove("viewer");
-    note.style.width = "200px";
-    note.style.height = "200px";
+  // event when modal is clicked
+
+  modal.addEventListener("click", function () {
     modal.style.display = "none";
-  };
+    tile.classList.remove("viewer");
+    tile.style.cssText = "width: 200px";
+    tile.style.cssText = "height: 200px";
+    text.classList.remove("text-viewer");
+    title.setAttribute("contentEditable", "false");
+    text.setAttribute("contentEditable", "false");
+  });
 });
