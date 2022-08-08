3eb7fcdae5e61d4824c2cf8d9bc803705297c34e
rambo <91745597+pranavxdev@users.noreply.github.com>
2022-08-08 21:17:05 +0400

    added remove button for each note

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ addNote.addEventListener("click", function () {
 
   //create note element
   const tile = document.createElement("div");
-  tile.setAttribute("id", "note");
+  tile.classList.add("note");
 
   // create note title
   const title = document.createElement("div");
@@ -19,10 +19,24 @@ addNote.addEventListener("click", function () {
   text.setAttribute("id", "text");
   text.innerHTML = "no additional text";
 
+  // create note nav
+  const noteNav = document.createElement("div");
+  noteNav.setAttribute("id", "noteNav");
+
+  // create delete button
+  const remove = document.createElement("div");
+  remove.setAttribute("id", "remove");
+
   //append title and text into parent NOTE and append NOTE into NoteField
+  noteNav.appendChild(remove);
+  tile.appendChild(noteNav);
   tile.appendChild(title);
   tile.appendChild(text);
   noteField.appendChild(tile);
+
+  remove.addEventListener("click", function () {
+    noteField.removeChild(tile);
+  });
 
   // event listener when tile is clicked
 
@@ -33,8 +47,11 @@ addNote.addEventListener("click", function () {
     tile.style.height = "500px";
     text.classList.add("text-viewer");
     title.setAttribute("contentEditable", "true");
+    title.style.cssText = "width: 100%";
+    title.style.marginLeft = "0";
     text.setAttribute("contentEditable", "true");
     text.removeAttribute("id");
+    remove.style.cssText = "display: none";
   });
 
   // event when modal is clicked
@@ -46,7 +63,9 @@ addNote.addEventListener("click", function () {
     tile.style.cssText = "height: 200px";
     text.classList.remove("text-viewer");
     title.setAttribute("contentEditable", "false");
+    title.style.cssText = "width: 150px";
     text.setAttribute("contentEditable", "false");
     text.setAttribute("id", "text");
+    remove.style.cssText = "display: initial";
   });
 });
